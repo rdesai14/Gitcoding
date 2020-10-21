e3c663848609485b41c65264353a165ddc81baa8
Your Name <you@example.com>
2020-10-21 14:37:35 -0400

    Added a Pig

--- sketch.js
+++ sketch.js
@@ -27,6 +27,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
+    pig4 = new Pig(820, 100);
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -54,6 +55,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
+    pig4.display();
 
     box5.display();
     log4.display();
